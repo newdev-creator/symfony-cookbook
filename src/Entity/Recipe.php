cf288598ec841ed594583e984f71e7bde257ca9c
Julien Freisa <newdevcreator@gmail.com>
2024-08-31 14:13:51 +0200

    FEAT: entities Recipe and User

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $personCount = null;
+
+    #[ORM\Column]
+    private ?int $prepTimeCount = null;
+
+    #[ORM\Column]
+    private ?int $cookTimeCount = null;
+
+    #[ORM\Column]
+    private ?int $rate = null;
+
+    #[ORM\Column]
+    private ?int $serving = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column]
+    private ?bool $isActive = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'recipes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPersonCount(): ?int
+    {
+        return $this->personCount;
+    }
+
+    public function setPersonCount(int $personCount): static
+    {
+        $this->personCount = $personCount;
+
+        return $this;
+    }
+
+    public function getPrepTimeCount(): ?int
+    {
+        return $this->prepTimeCount;
+    }
+
+    public function setPrepTimeCount(int $prepTimeCount): static
+    {
+        $this->prepTimeCount = $prepTimeCount;
+
+        return $this;
+    }
+
+    public function getCookTimeCount(): ?int
+    {
+        return $this->cookTimeCount;
+    }
+
+    public function setCookTimeCount(int $cookTimeCount): static
+    {
+        $this->cookTimeCount = $cookTimeCount;
+
+        return $this;
+    }
+
+    public function getRate(): ?int
+    {
+        return $this->rate;
+    }
+
+    public function setRate(int $rate): static
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getServing(): ?int
+    {
+        return $this->serving;
+    }
+
+    public function setServing(int $serving): static
+    {
+        $this->serving = $serving;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setActive(bool $isActive): static
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
