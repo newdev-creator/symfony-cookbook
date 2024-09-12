465034690538f2044abc25f08b2f21a31380230e
Julien Freisa <newdevcreator@gmail.com>
2024-09-12 09:51:25 +0200

    FEAT: get three recipes with the max rate

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,29 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('', name: 'home_index')]
-    public function index(): Response
+    public function index(RecipeRepository $recipeRepository): Response
     {
+        // Get 3 random recipe with max rate
+        // Find max rate on bdd
+        $maxRate = $recipeRepository->findMaxRate();
+        
+        $recipes = [];
+
+        // Stock 3 recipes with rate equal to $maxRate
+        if ($maxRate !== null) {
+            $recipes = $recipeRepository->findRandomRecipeByRate($maxRate, 3);
+
+            // if $recipes[] dont full, get other recipes with $maxRate - 1
+            if (count($recipes) < 3) {
+                for ($rate=$maxRate - 1; $rate >= 1 && count($recipes) < 3; $rate--) { 
+                    $addRecipes = $recipeRepository->findRandomRecipeByRate($rate, 3 - count($recipes));
+                    $recipes = array_merge($recipes, $addRecipes);
+                }
+            }
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'recipes' => $recipes,
         ]);
     }
 }
--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -16,28 +16,40 @@ class RecipeRepository extends ServiceEntityRepository
         parent::__construct($registry, Recipe::class);
     }
 
-//    /**
-//     * @return Recipe[] Returns an array of Recipe objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findMaxRate(): ?int
+    {
+        return $this->createQueryBuilder('r')
+            ->select('MAX(r.rate) as maxRate')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
 
-//    public function findOneBySomeField($value): ?Recipe
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findRandomRecipeByRate(int $rate, int $limit = 3): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+            SELECT * FROM recipe r 
+            WHERE r.rate = :rate 
+            ORDER BY RAND() 
+            LIMIT ' . (int) $limit;
+
+        $resultSet = $conn->executeQuery($sql, [
+            'rate' => $rate,
+            'limit' => $limit,
+        ]);
+
+        return $resultSet->fetchAllAssociative();
+    }
+
+    // public function findRandomRecipeByRate(int $rate, int $limit = 3): array
+    // {
+    //     return $this->createQueryBuilder('r')
+    //         ->where('r.rate = :rate')
+    //         ->setParameter('rate', $rate)
+    //         ->orderBy('RAND()')
+    //         ->setMaxResults($limit)
+    //         ->getQuery()
+    //         ->getResult();
+    // }
 }
